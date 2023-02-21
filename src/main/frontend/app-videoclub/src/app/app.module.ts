6b2d478606bdc9b3e8d28ca76458bfab3507a5ab
Jchabol1206 <juachabol@gmail.com>
2023-02-21 12:53:58 +0100

    inicio crud pelicula, dejar para despues del examen

--- src/main/frontend/app-videoclub/src/app/app.module.ts
+++ src/main/frontend/app-videoclub/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {CategoriaModule} from "./categoria/categoria.module";
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
+import {PeliculaModule} from "./pelicula/pelicula.module";
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import {HttpClientModule} from "@angular/common/http";
     NgbModule,
     CategoriaModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    PeliculaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
